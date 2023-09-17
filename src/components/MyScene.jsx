3be1a5c00e9448b12eb10d643c58a4051407569f
kku98057 <kku98057@naver.com>
2023-09-17 21:37:12 +0900

    update scroll msg

--- src/components/MyScene.jsx
+++ src/components/MyScene.jsx
@@ -1,12 +1,40 @@
-import { ScrollControls } from "@react-three/drei";
+import { Scroll, ScrollControls, useScroll } from "@react-three/drei";
 import { Crystal } from "../../public/model/Crystal";
 import Background from "./Background";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import gsap from "gsap";
 
 export default function MyScene() {
+  const [scrollMsg, setScrollMsg] = useState(true);
+
   return (
     <>
+      <ScrollMessage />
       <Crystal />
       <Background />
     </>
   );
 }
+
+const ScrollMessage = () => {
+  const scroll = useScroll();
+  const [scrollOpacity, setScrollOpacity] = useState(1);
+  const scrollRef = useRef();
+  const tl = useRef();
+
+  useFrame(() => {
+    setScrollOpacity(1 - scroll.range(0, 1 / 64));
+  });
+
+  return (
+    <Scroll html ref={scrollRef} style={{ opacity: scrollOpacity }}>
+      <div className="scroll_msg">
+        <div className="bar">
+          <div></div>
+        </div>
+        <h1>to scroll</h1>
+      </div>
+    </Scroll>
+  );
+};
